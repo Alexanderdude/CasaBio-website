4b4fd69504389a338574dfb8ca819760b7e1c67e
Alexander Franz <59662966+Alexanderdude@users.noreply.github.com>
2023-09-18 11:47:19 +0200

    Added Files via Upload
    Added a basic register user form and customised the login form.

--- reactjs-frontend/src/Login.js
+++ reactjs-frontend/src/Login.js
@@ -2,32 +2,36 @@
 import { useState } from 'react';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
+import './Login.css'; // Import the CSS file
 
 function Login(props) {
 
-    //sets the useState for the login details as blank strings
-    const [loginForm, setloginForm] = useState({
-      username: "",
-      password: ""
+  //sets the useState for the login details as blank strings
+  const [loginForm, setloginForm] = useState({
+    username: "",
+    password: ""
+  })
+
+  //defines navigate for a successful login
+  const navigate = useNavigate();
+
+  //sets a variable to change the form if the user is registering
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  //defines the dunction logMeIn
+  function logMeIn(event) {
+
+    //sends a POST request to the /token api using axios
+    axios({
+      method: "POST",
+      url: "/token",
+
+      //retrieve data from backend
+      data: {
+        username: loginForm.username,
+        password: loginForm.password
+      }
     })
-
-    //defines navigate for a successful login
-    const navigate = useNavigate();
-
-    //defines the dunction logMeIn
-    function logMeIn(event) {
-
-        //sends a POST request to the /token api using axios
-      axios({
-        method: "POST",
-        url:"/token",
-
-        //retrieve data from backend
-        data:{
-          username: loginForm.username,
-          password: loginForm.password
-         }
-      })
 
       //iff successful then navigate to profile and set the token as the accesstoken
       .then((response) => {
@@ -40,59 +44,231 @@ function Login(props) {
           console.log(error.response)
           console.log(error.response.status)
           console.log(error.response.headers)
-          }
+        }
       })
 
-      //sets the loginForm to blank strings
-      setloginForm(({
-        username: "",
-        password: ""}))
+    //sets the loginForm to blank strings
+    setloginForm(({
+      username: "",
+      password: ""
+    }))
 
-        //prevents the event to work if values are default
-      event.preventDefault()
+    //prevents the event to work if values are default
+    event.preventDefault()
+  }
+
+  //handleChange event
+  function handleChange(event) {
+
+    // Destructure the 'value' and 'name' properties from the event target
+    const { value, name } = event.target
+
+    //update the form to replace the name value with the new name value
+    setloginForm(prevNote => ({
+      ...prevNote, [name]: value
+    })
+    )
+  }
+
+  //function to change the isRegistering variable 
+  function handleToggleRegister() {
+    setIsRegistering(!isRegistering);
+  }
+
+  function registerUser(event) {
+    event.preventDefault();
+    // Check if password and confirm password match
+    if (loginForm.password !== loginForm.confirmPassword) {
+      alert("Password and Confirm Password must match.");
+      return;
+    }
+  
+    // Prepare the user data for registration
+    const userData = {
+      username: loginForm.username,
+      email: loginForm.email,
+      password: loginForm.password,
+      securityQuestion: loginForm.securityQuestion
+    };
+  
+    // Send a POST request to your Flask /register endpoint
+    axios({
+      method: "POST",
+      url: "/register",
+      data: userData,
+    })
+      .then((response) => {
+        // Handle the successful registration response, e.g., store the access token
+        props.setToken(response.data.access_token);
+        navigate('/profile');
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.msg); // Display the error message from the server
+          return;
+        }
+      });
+  
+    // Toggles the registering values
+    handleToggleRegister();
+    
+    // Reset the registration form
+    setloginForm({
+      username: "",
+      password: "",
+      confirmPassword: "",
+      email: "",
+      securityQuestion: ""
+    });
+  }
+
+  //defines the password strength variable and timer
+  const [passwordStrength, setPasswordStrength] = useState('');
+  const [timer, setTimer] = useState(null);
+
+  //function that checks the password strength
+  function checkPasswordStrength(password) {
+    let strength = '';
+
+    //displays the message if password is less than 8 in length
+    if (password.length < 8) {
+      strength = 'Password should be at least 8 characters long';
     }
 
-    //handleChange event
-    function handleChange(event) { 
+    return strength;
+  }
 
-        // Destructure the 'value' and 'name' properties from the event target
-      const {value, name} = event.target
+  //function that handles the password change
+  function handlePasswordChange(event) {
+    clearTimeout(timer); // Clear any existing timer
+    const password = event.target.value;
 
-      //update the form to replace the name value with the new name value
-      setloginForm(prevNote => ({
-          ...prevNote, [name]: value})
-      )}
+    // Set a timer to update the password strength after a brief pause (e.g., 500ms)
+    const newTimer = setTimeout(() => {
+      const strength = checkPasswordStrength(password);
+      setPasswordStrength(strength);
+    }, 500);
 
-    return (
-      <div>
-        {/* basic login page to be updated */}
+    setTimer(newTimer);
 
-        <h1>Login</h1>
+    //handle the onchange event
+    handleChange(event);
+  }
 
-        {/* Input fields */}
-          <form className="login">
+  return (
+    <div className="centered">
 
-            {/* handleChange event */}
-            <input onChange={handleChange} 
-                  type="username"
-                  text={loginForm.username} 
-                  name="username" 
-                  placeholder="Username" 
-                  value={loginForm.username} />
+      {/* Displays Heading text based on Login or Registering */}
+      <h1>{isRegistering ? 'Register' : 'Login'}</h1>
 
+      {/* Creates the Login Form */}
+      <form className="login">
 
-            <input onChange={handleChange} 
-                  type="password"
-                  text={loginForm.password} 
-                  name="password" 
-                  placeholder="Password" 
-                  value={loginForm.password} />
+        <div className="form-group">
 
-            {/* Button to handle the logMeIn function when clicked */}
-          <button onClick={logMeIn}>Submit</button>
-        </form>
-      </div>
-    );
+          {/* Username Input field */}
+          <label htmlFor="username">Username:</label>
+          <input
+            //Settings for the input box
+            onChange={handleChange}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            value={loginForm.username}
+          />
+        </div>
+
+        <div className="form-group">
+
+          {/* Password Input field */}
+          <label htmlFor="password">Password:</label>
+          <input
+            //Settings for the input box
+            onChange={handlePasswordChange}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            value={loginForm.password}
+          />
+          {/* Checks if the user is registering or not */}
+          {isRegistering && passwordStrength && (
+            <div className="password-strength">
+              {passwordStrength}
+            </div>
+          )}
+        </div>
+
+        {/* If the user is registering */}
+        {isRegistering && (
+          <>
+            <div className="form-group">
+
+              {/*  */}
+              <label htmlFor="confirmPassword">Confirm Password:</label>
+              <input
+                //Settings for the input box
+                onChange={handleChange}
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                value={loginForm.confirmPassword}
+              />
+            </div>
+
+            <div className="form-group">
+
+              {/* Email Input field */}
+              <label htmlFor="email">Email:</label>
+              <input
+                //Settings for the input box
+                onChange={handleChange}
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                value={loginForm.email}
+              />
+            </div>
+
+            <div className="form-group">
+
+              {/* Question Input field */}
+              <label htmlFor="securityQuestion">What is your reason for joining CASABIO:</label>
+              <input
+                //Settings for the input box
+                onChange={handleChange}
+                type="text"
+                name="securityQuestion"
+                id="securityQuestion"
+                placeholder="answer"
+                value={loginForm.securityQuestion}
+              />
+            </div>
+          </>
+        )}
+
+        {/* Adds a button that has multiple functions if user is registering or not */}
+        <button onClick={isRegistering ? registerUser : logMeIn}>
+          {isRegistering ? 'Register' : 'Login'}
+        </button>
+      </form>
+
+      <p>
+        {/* Adds some text that asks the user if they have an account or not */}
+        {isRegistering
+          ? 'Already have an account?'
+          : "Don't have an account yet?"}
+
+        {/* Adds a button that toggles the registering variable */}
+        <button onClick={handleToggleRegister}>
+          {isRegistering ? 'Login' : 'Register'}
+        </button>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
